94731095cc50222507cabbd4318980ab70573b86
CevdetTellioglu <cevdettellioglu@MacBook-Pro-3.local>
2020-06-22 21:05:56 +0200

    22062020 ikinci guncelleme

---
+++ src/Day2_VerifyURLTestRV.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day2_VerifyURLTestRV {
+    public static void main(String[] args) {
+        //1. Yeni bir class oluşturun : VerifyURLTest
+        //         (URLDoğrulamaTesti)
+        //2. Navigate to google homepage
+        //3. Google ana sayfa URL'sinin “www.google.com” olduğunu doğrulayın(verify)
+        System.setProperty("webdriver.chrome.driver","/Users/cevdettellioglu/Documents/selenium libraries/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://www.google.com");
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "www.google.com";
+        if(actualURL.equals(expectedURL)){
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAIL "+ "\n Actuel url : "+actualURL);
+        }
+    }
+}
---
+++ src/com/techproedpackage/Day3_LocatorsIntroRV.java
@@ -0,0 +1,65 @@
+package com.techproedpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class Day3_LocatorsIntroRV {
+    public static void main(String[] args) {
+//Bir class oluşturun: LocatorsIntro
+        //Main method oluşturun ve aşağıdaki görevi tamamlayın.
+        //        http://a.testaddressbook.com/sign_in  adresine gidiniz.
+        //email textbox,password textbox, and signin button elementlerini locate ediniz..
+        //Kullanıcı adını ve şifreyi aşağıya girin ve oturum aç (sign in)buttonunu tıklayın:
+        //Username :  testtechproed@gmail.com
+        // Password : Test1234!
+
+        System.setProperty("webdriver.chrome.driver","/Users/cevdettellioglu/Documents/selenium libraries/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://a.testaddressbook.com/sign_in");
+        // email textbox locate
+        WebElement emailTextBox = driver.findElement(By.id("session_email"));
+        emailTextBox.sendKeys("testtechproed@gmail.com");
+        //password textbox locate
+        WebElement passwordTextBox = driver.findElement(By.id("session_password"));
+        passwordTextBox.sendKeys("Test1234!");
+        //signin button elementlerini locate
+        WebElement singInButton = driver.findElement(By.name("commit"));
+        singInButton.click();
+
+        WebElement userId = driver.findElement(By.className("navbar-text"));
+        System.out.println("USER ID WEBELEMENT ==>"+userId);
+        System.out.println("USER ID TEXT => "+userId.getText());
+        String actualUserId = userId.getText();
+        String expectedUserId = "testtechproed@gmail.com";
+        if(actualUserId.equals(expectedUserId)) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
+        //“Addresses” ve “Sign Out” textlerinin görüntülendiğini( displayed) doğrulayin(verify).
+        WebElement addressLink = driver.findElement(By.linkText("Addresses"));
+        String actualAddressText = addressLink.getText();
+        String expectedAddressesText = "Addresses";
+        if(actualAddressText.equals(expectedAddressesText)){
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
+        WebElement signOutLink = driver.findElement(By.linkText("Sign out"));
+        String actualSignOutLink = signOutLink.getText();
+        String expectedSignOutLink = "Sign Out";
+        if(actualSignOutLink.equals(expectedSignOutLink)){
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
+        List<WebElement> links = driver.findElements(By.xpath("<a>"));
+        int linkCount = links.size();
+        System.out.println(driver.getCurrentUrl()+ " sayfasındaki toplam link sayısı = "+ linkCount +"'dir");
+
+    }
+}
